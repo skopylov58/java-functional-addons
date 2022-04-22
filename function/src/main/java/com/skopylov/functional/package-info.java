3591c72b6f4614fd466bcd9759cd5a760d000e2b
Sergey Kopylov <sergey.kopylov@hpe.com>
2022-04-22 16:23:17 +0300

    fixing commits

--- function/src/main/java/com/skopylov/functional/package-info.java
+++ function/src/main/java/com/skopylov/functional/package-info.java
@@ -1,4 +1,4 @@
 /**
- * This package provides functional interfaces which throws Exceptions and some useful  
+ * This package provides interfaces for functional exception handling {@code Try<T>} {@code Either<R, L>}.
  */
 package com.skopylov.functional;
