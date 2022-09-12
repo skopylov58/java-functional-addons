927a978049311dcb7a20dbd428ce1e370a7d9ac2
Sergey Kopylov <skopylov@gmail.com>
2022-09-12 16:57:49 +0300

    feat: Higher-order functions to handle exceptions

---
+++ function/src/main/java/com/github/skopylov58/functional/TryUtils.java
@@ -0,0 +1,121 @@
+package com.github.skopylov58.functional;
+
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+/**
+ * Collection of higher-order functions to handle exceptions in functional style.
+ * 
+ * @author skopylov@gmail.com
+ *
+ */
+public interface TryUtils {
+
+    @FunctionalInterface
+    interface CheckedFunction<T, R > {
+        R apply(T t) throws Exception;
+    }
+
+    @FunctionalInterface
+    interface CheckedSupplier<T> {
+        T get() throws Exception;
+    }
+
+    /** Try Result for Java 8+ */
+    public static class ResultJava8<T> {
+        private final Object value;
+        public ResultJava8(T t) {value = t;}
+        public ResultJava8(Exception e ) {value = e;}
+        public boolean failed () {return value instanceof Exception;} 
+        public T result() {return (T) value;}
+        public Exception cause() {return (Exception) value;}
+    }
+
+    /** Try Result for Java 14+ */
+    record Result<T>(T result, Exception exception) {
+        public boolean failed() {return exception != null;}
+        public Stream<T> stream() {return failed() ? Stream.empty() : Stream.of(result);}
+    } 
+
+    /**
+     * Higher-order function to convert partial function {@code T=>R} to total function {@code T=>Result<R>}
+     * @param <T> function input parameter type
+     * @param <R> function result type
+     * @param func partial function T=>R that may throw checked exception
+     * @return total function {@code T => Result<R>}
+     */
+    static <T, R> Function<T, Result<R>> toResult(CheckedFunction<T, R> func) {
+        return param -> {
+            try {
+                return new Result<>(func.apply(param), null);
+            } catch (RuntimeException re) {
+                throw re;
+            } catch (Exception e) {
+                return new Result<>(null, e);
+            }
+        };
+    }
+    
+    /**
+     * Higher-order function to convert partial function {@code T=>R} to total function {@code T=>Optional<R>}
+     * @param <T> function input parameter type
+     * @param <R> function result type
+     * @param func partial function {@code T => R} that may throw checked exception
+     * @return total function {@code T => Optional<R>}
+     */
+    static <T, R> Function<T, Optional<R>> toOptional(CheckedFunction<T, R> func) {
+        return param -> {
+            try {
+                return Optional.ofNullable(func.apply(param));
+            } catch (RuntimeException re) {
+                throw re;
+            } catch (Exception e) {
+                return Optional.empty();
+            }
+        };
+    }
+
+    /**
+     * Higher-order function to convert partial supplier {@code () => T} to total supplier {@code () => Optional<T>}
+     * @param <T> supplier result type
+     * @param supplier {@code () => T} that may throw an exception
+     * @return total supplier {@code () => Optional<T>}
+     */
+    static <T> Supplier<Optional<T>> toOptional(CheckedSupplier<T> supplier) {
+        return () -> {
+            try {
+                return Optional.ofNullable(supplier.get());
+            } catch (Exception e) {
+                return Optional.empty();
+            }
+        };
+    }
+    /**
+     * Higher-order function to convert partial function {@code T => R} to total function {@code T=>Either<R, Exception>}
+     * @param <T> function input parameter type
+     * @param <R> function result type
+     * @param func partial function {@code T => R} that may throw an exception
+     * @return total function {@code T => Either<R, Exception>}
+     */
+    static <T, R> Function<T, Either<R, Exception>> toEither(CheckedFunction<T, R> func) {
+        return param -> {
+            try {
+                return Either.right(func.apply(param));
+            } catch (Exception e) {
+                return Either.left(e);
+            }
+        };
+    }
+
+    static <T, R> Function<T, ResultJava8<R>> toResultJava8(CheckedFunction<T, R> func) {
+        return param -> {
+            try {
+                return new ResultJava8<>(func.apply(param));
+            } catch (Exception e) {
+                return new ResultJava8<>(e);
+            }
+        };
+    }
+}
---
+++ function/src/test/java/com/github/skopylov58/functional/TryUtilsTest.java
@@ -0,0 +1,125 @@
+package com.github.skopylov58.functional;
+
+import static com.github.skopylov58.functional.TryUtils.toEither;
+import static com.github.skopylov58.functional.TryUtils.toOptional;
+import static com.github.skopylov58.functional.TryUtils.toResult;
+import static com.github.skopylov58.functional.TryUtils.toResultJava8;
+import static org.junit.Assert.assertTrue;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.NumberFormat;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+import org.junit.Test;
+
+import com.github.skopylov58.functional.TryUtils.Result;
+
+public class TryUtilsTest {
+
+    final List<String> URLS = List.of("www.google.com", "foo.bar", "http://a.b.c");
+    final String[] NUMS = new String[]{"foo", "bar", "3", "4.9"};
+    
+    @Test
+    public void testWithOptional() throws Exception {
+        List<URL> list = URLS.stream()
+        .map(toOptional(URL::new))
+        .flatMap(Optional::stream)
+        .toList();
+        check(list);
+    }
+
+    void check(List<URL> list) throws MalformedURLException {
+        System.out.println(list);
+        assertTrue(list.size() == 1);
+        assertTrue(list.get(0).equals(new URL("http://a.b.c")));
+    }
+    
+    @Test
+    public void testWithEither() throws Exception {
+        List<URL> res = URLS.stream()
+            .map(toEither(URL::new))
+            .peek(either -> {if (either.isLeft()) System.out.println(either.getLeft());})
+            .filter(Either::isRight)
+            .map(Either::getRight)
+            .toList();
+        check(res);
+    }
+
+    @Test
+    public void testWithResultJava8() throws Exception {
+        List<URL> res = URLS.stream()
+            .map(toResultJava8(URL::new))
+            .peek(r -> {if (r.failed()) System.out.println(r.cause());})
+            .filter(Predicate.not(TryUtils.ResultJava8::failed))
+            .map(r -> r.result())
+            .toList();
+        check(res);
+    }
+
+    @Test
+    public void testWithResultRecord() throws Exception {
+        var res = URLS.stream()
+            .map(toResult(URL::new))
+            .filter(rec -> !rec.failed())
+            .map(Result::result)
+            .toList();
+        
+        check(res);
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void intList() throws NumberFormatException {
+        var list = Stream.of(NUMS)
+        .map(toOptional(Integer::parseInt)) //Runtime NumberFormatException may happen here
+        .flatMap(Optional::stream)
+        .toList();
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void intListWithResult() throws NumberFormatException {
+        var list = Stream.of(NUMS)
+        .map(toResult(Integer::parseInt)) //Runtime NumberFormatException may happen here
+        .peek(this::handleErr) 
+        .flatMap(Result::stream)
+        .toList();
+    }
+    
+    @Test
+    public void intListWithNumberFormat() {
+        NumberFormat format = NumberFormat.getInstance();
+        var list = Stream.of(NUMS)
+        .map(toOptional(format::parse)) //Checked ParseException may happen here
+        .flatMap(Optional::stream)
+        .map(Number::intValue)
+        .toList();
+        System.out.println(list);
+    }
+    
+    void handleErr(Result r) {
+        if (r.failed()) {
+            System.out.println(r.exception());
+        }
+    }
+    
+    List<Number> intListWithResult(String [] numbers) {
+        NumberFormat format = NumberFormat.getInstance();
+        return Stream.of(numbers)      //Stream<String>
+        .map(toResult(format::parse))  //Stream<Result<Number>>, ParseException may happen
+        .peek(this::handleErr)         //Stream<Result<Number>>
+        .flatMap(Result::stream)       //Stream<Number>
+        .toList();                     //List<Number>
+    }
+
+    
+//    @Test
+//    public void testSocket() throws Exception {
+//        Optional.of("host")
+//        .flatMap(TryUtils.toOptional(h -> new Socket(h, 8848)))
+//        .flatMap(TryUtils.toOptional(Socket::getOutputStream))
+//        .ifPresent(TryUtils.toOptional(out -> out.write(new byte[] {1,2,3})));
+//    }
+}
