42fa17dabd23071c5aedfa87d6ed04ee05fd6de1
Sergey Kopylov <skopylov@gmail.com>
2022-09-13 11:00:55 +0300

    javadoc: updates

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.github.skopylov58.functional"},{"l":"com.github.skopylov58.jdbc"},{"l":"com.github.skopylov58.jdbc.pool"},{"l":"com.github.skopylov58.retry"}];updateSearchResults();
