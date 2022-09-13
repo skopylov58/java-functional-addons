19569069c755761453df3ae88471a1a80d144a8d
Sergey Kopylov <skopylov@gmail.com>
2022-09-13 10:43:28 +0300

    javadoc: cleanup

--- docs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.skopylov.functional"},{"l":"com.skopylov.jdbc"},{"l":"com.skopylov.jdbc.pool"},{"l":"com.skopylov.retry"}];updateSearchResults();
