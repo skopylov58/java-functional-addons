24697ee04a44cb961f2a5d3648ef5581c67c2630
Sergey Kopylov <skopylov@gmail.com>
2022-09-14 16:24:33 +0300

    test: removed unused import

--- function/src/test/java/com/github/skopylov58/functional/OptionalTest.java
+++ function/src/test/java/com/github/skopylov58/functional/OptionalTest.java
@@ -6,8 +6,6 @@ import java.math.BigDecimal;
 import java.util.Optional;
 import java.util.function.Function;
 
-import javax.annotation.processing.SupportedSourceVersion;
-
 import org.junit.Test;
 
 public class OptionalTest {
