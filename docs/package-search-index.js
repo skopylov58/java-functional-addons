c37d934657c9259fefb8a9466dd5fcc6c771fadf
Sergey Kopylov <skopylov@gmail.com>
2023-03-24 14:51:22 +0300

    build: deleted root docs

--- docs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.github.skopylov58.functional"},{"l":"com.github.skopylov58.jdbc"},{"l":"com.github.skopylov58.jdbc.pool"},{"l":"com.github.skopylov58.retry"}];updateSearchResults();
